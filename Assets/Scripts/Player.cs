a624d2abee60fd2bdcc31db6dc2a2f0af15f4cd7
Najaehyeon <jaehyeon4832@gmail.com>
2025-02-22 17:28:13 +0900

    필요한 것 주석

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // 필요 기능: 플레이어 움직임(점프, 슬라이드) => 점프하고 슬라이드 할 때 애니메이션
+    // 필요 요소: 플레이어 점프 파워 조절, 슬라이드 시간 조절, 체력
+}
