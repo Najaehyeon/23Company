cb8192f2ca9cd1d14b1da35ac79067019441ed4a
XPOW <zug3049@gmail.com>
2025-02-24 12:24:02 +0900

    Update: ScrollBG, obstacle random
    배경 교차 스크롤 기능, 장애물 랜덤 생성

--- Assets/Scripts/BGLooper.cs
+++ Assets/Scripts/BGLooper.cs
@@ -2,20 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class BGLopper : MonoBehaviour
 {
     public int numBgCount = 5;
 
-    private void Start()
-    {
-        
+    public int obstacleCount = 0;
+    public Vector3 obstacleLastPosition = Vector3.zero;
+
+     void Start()
+    {   /*
+        Obstacle[] obstacles = GameObject.FindObjectOfType<Obstacle>();
+        obstacleLastPosition = obstacles[0].transform.position;
+        obstacleCount = obstacles.Length;
+
+        for(int i = 0; i < obstacleCount; i++)
+        {
+            obstacleLastPosition = obstacles[i].SetRandomPlace(obstacleLastPosition, obstacleCount);
+        }
+        */
     }
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
-       
-
         Debug.Log("Triggerd: " +  collision.name);
         if (collision.CompareTag("BackGround"))
         {
@@ -27,7 +37,12 @@ public class BGLopper : MonoBehaviour
             collision.transform.position = pos;
             return;
         }
+        /*
+        Obstacle obstacle = collision.GetComponent<Obstacle>();
+        if (obstacle)
+        {
+            obstacleLastPosition = obstacle.SetRandomPlace(obstacleLastPosition, obstacleCount);
+        }
+        */
     }
-
-    
 }
