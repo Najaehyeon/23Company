e6f0876b5525ab43b9f0ec929b14dfccc5f6af12
pxo2 <rlarbxodia@gmail.com>
2025-02-25 12:09:39 +0900

    Update_gyutae
    2단점프 완료

---
+++ Assets/Scripts/BaseController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseController : MonoBehaviour
+{
+    protected Rigidbody2D _rigidbody;
+
+    [SerializeField] private SpriteRenderer characterRenderer;
+
+    protected Vector2 movementDirection = Vector2.zero;
+    public Vector2 MovementDirection { get { return movementDirection; } }
+
+    protected AnimationHandler animationHandler;
+
+    protected virtual void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        animationHandler = GetComponent<AnimationHandler>();
+    }
+
+    protected virtual void Start()
+    {
+
+    }
+
+    protected virtual void Update()
+    {
+        HandleAction();
+    }
+
+    protected virtual void FixedUpdate()
+    {
+        Movment(movementDirection);
+
+    }
+
+    protected virtual void HandleAction()
+    {
+
+    }
+
+    private void Movment(Vector2 direction)
+    {
+        direction = direction * 5;
+
+        _rigidbody.velocity = direction;
+        animationHandler.Jump(direction);
+    }
+}
