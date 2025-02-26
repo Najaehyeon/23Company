7a51825edda42f19b9a37de6968932c3445b8e45
Bosung <gobosung2000@gmail.com>
2025-02-26 11:07:24 +0900

    [Fix] 중간결합 완료 >장애물 충돌처리/슬라이더 /UI 빼고 나머지 완료

--- Assets/Scripts/Item/EffectItem.cs
+++ Assets/Scripts/Item/EffectItem.cs
@@ -25,59 +25,19 @@ public class EffectItem : Item
     // 아이템 효과 적용
     private void ApplyEffect(Player player)
     {
-        // switch (itemType)
-        // {
-        //     case ItemType.Chicken:
-        //         player.Heal(10);    // 체력 10 회복
-        //         break;
-        //     case ItemType.Fireball:
-        //         player.SpeedUp(2f, 5f); // 속도 2 증가, 5초 후 원래 속도로 복귀
-        //         break;
-        //     case ItemType.Poison:
-        //         player.StartCoroutine(player.TakePoisonDamage(3, 5, 0.5f)); // 3 데미지, 5번 반복, 0.5초 간격
-        //         break;
-        // }
+        switch (itemType)
+        {
+            case ItemType.Chicken:
+                player.Heal(10);    // 체력 10 회복
+                break;
+            case ItemType.Fireball:
+                player.SpeedUp(2f, 5f); // 속도 2 증가, 5초 후 원래 속도로 복귀
+                break;
+            case ItemType.Poison:
+                player.StartCoroutine(player.TakePoisonDamage(3, 5, 0.5f)); // 3 데미지, 5번 반복, 0.5초 간격
+                break;
+        }
     }
     
-    // //플레이어 클래스에 넣어주기
-    // public void Heal(int amount)    // 플레이어 HP 회복
-    // {
-    //     currentHealth += amount;  // 체력 회복
-    //     if (currentHealth >= maxHealth) 
-    //     {
-    //         currentHealth = maxHealth; // 최대 체력 초과 방지
-    //     }
-    // }
-    //
-    // //플레이어 클래스에 넣어주기
-    // public void SpeedUp(float amount, float duration)   // 플레이어 속도 증가
-    // {
-    //     speed += amount; // 속도 증가
-    //     Invoke(nameof(ResetSpeed), duration);   // 일정 시간 후 원래 속도로 복귀
-    // }
-    // private void ResetSpeed()   // 속도를 원래대로 초기화
-    // {
-    //     speed -= 2f; // 원래 속도로 복귀(규태님 나중에 2f 대신 플레이어 기본 속도로 넣어주세여)
-    // }
-    //
-    // //플레이어 클래스에 넣어주기
-    // public void TakeDamage(int amount)
-    // {
-    //     currentHealth -= amount;  // 체력 감소
-    //     if (currentHealth <= 0)
-    //     {
-    //         currentHealth = 0; // 체력이 0 이하로 내려가지 않도록 방지
-    //     }
-    // }
-    //
-    // //플레이어 클래스에 넣어주기
-    // public IEnumerator TakePoisonDamage(int damage, int times, float interval)  // 일정 시간마다 HP 감소하는 코루틴
-    // {
-    //     for (int i = 0; i < times; i++)
-    //     {
-    //         TakeDamage(damage);
-    //         Debug.Log($"독 데미지! 피해 {damage}, 현재 체력 {currentHealth}, 남은 횟수 {times - i - 1}번");
-    //         yield return new WaitForSeconds(interval);  // n초(interval) 대기
-    //     }
-    // }
+    
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,5 +5,237 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    
+    Animator animator; // 유니티에서 가져오기
+    Rigidbody2D _rigidbody;
+
+
+    public float playerJumpPower = 15f; // 점프하는 힘
+    public float forwardSpeed = 0f; // 전진 속도
+    public float forwardSpeed_before = 0f; // 전진 속도
+    public float currentHealth = 0f;
+    public float maxHealth = 100f;
+
+
+    public bool isDead = false; // 생사여부 확인
+    float deathCooldown = 0f; // 죽는 모션 딜레이
+    public bool slide; // 슬라이드 작동
+    private bool ground;
+    private int jumpCount = 0;
+    float lastPosition_Y = 0f;
+    bool isRun = false; // 점프유무 체크 요소 // 기본행동
+    float Position = 0f;
+
+    ItemManager itemManager;
+
+    // Start is called before the first frame update
+    void Start()
+    { // InChildren 을 붙여 하위 오브젝트들에게도 탐색 적용
+        animator = GetComponentInChildren<Animator>(); // 작성중인 스크립트가 부착된 오브젝트에게 내가 찾고있는 
+        _rigidbody = GetComponent<Rigidbody2D>(); // 컴포넌트가 있는지 탐색후 반환
+        itemManager = ItemManager.Instance;
+
+        if (animator == null)
+            Debug.Log("ani error");
+
+        if (_rigidbody == null)
+            Debug.Log("rigid error");
+
+        //플레이어 위치 초기화 
+        gameObject.transform.position = new Vector3(0f, -4.8f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        JumpCheck();
+
+        AddScore_position();
+    }
+
+    private void AddScore_position()
+    {
+        Position = gameObject.transform.position.x;
+
+        if (Position % 1 == 0)
+        {
+            itemManager.AddScore(1);
+
+        }
+    }
+
+    private void JumpCheck()
+    {
+        ground = CheckGround();
+
+        if (ground) // 바닥에 있을때
+        {
+            jumpCount = 0; // 점프횟수 초기화
+        }
+
+        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) && jumpCount < 2)
+        { // 2단 점프 제한
+            Jump();
+            jumpCount++;
+
+            ground = false;
+        }
+
+        if ((Input.GetKey(KeyCode.DownArrow) || Input.GetMouseButtonDown(1)) && ground)
+        {
+            Slide(true);
+        }
+        else
+        {
+            Slide(false);
+        }
+
+
+        if (isDead)
+        {
+            if (deathCooldown <= 0f) // 죽은경우 재시작
+            {
+
+            }
+            else
+            {
+                deathCooldown -= Time.deltaTime;
+            }
+        }
+        else // 죽지 않은 상태 
+        {
+            if ((Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) && jumpCount < 2)
+            {
+                isRun = true;
+            }
+        }
+    }
+
+    private void FixedUpdate() // 물리 연산이 필요할 때 일정한 간격으로 호출
+    {
+        if (isDead) return; // isDead 가 true 라면 작업하지 않고 반환
+
+        Vector3 velocity = _rigidbody.velocity; // _rigidbody 의 가속도 가져오기
+        velocity.x = forwardSpeed;
+
+        if (isRun) // isRun 이 true 일때
+        {
+            velocity.y += playerJumpPower; // velocity.y 에 점프할때 가속 적용
+            //velocity.y = playerJump; // velocity.y 에 점프하는 힘 넣기 차이가 뭐지
+            isRun = false;
+        }
+
+        _rigidbody.velocity = velocity; // 다시 넣어줘야함
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) // 충돌에 대한 이벤트 발생시 실행
+    {
+        if (isDead) return; // 이미 죽었으면 충돌 무시
+
+        if (collision.gameObject.CompareTag("Ground"))  // 땅과 충돌하면 죽지 않도록 예외 처리
+        {
+            ground = true;   // 바닥 감지
+            jumpCount = 0;   // 점프 횟수 초기화
+            return;          // 죽지 않도록 예외 처리
+        }
+        //isDead = true;
+        //deathCooldown = 1f;
+        //animator.SetInteger("IsDie", 1); // 애니메이터에 "IsDie"라는 파라미터의 값을 1로 설정(블럭연결)
+
+    }
+
+    //플레이어 클래스에 넣어주기
+    public void Heal(int amount)    // 플레이어 HP 회복
+    {
+        currentHealth += amount;  // 체력 회복
+        if (currentHealth >= maxHealth)
+        {
+            currentHealth = maxHealth; // 최대 체력 초과 방지
+        }
+        Debug.Log("체력추가");
+    }
+
+    //플레이어 클래스에 넣어주기
+    public void SpeedUp(float amount, float duration)   // 플레이어 속도 증가
+    {
+        forwardSpeed_before = forwardSpeed;
+        forwardSpeed += amount; // 속도 증가
+        Invoke(nameof(ResetSpeed), duration);   // 일정 시간 후 원래 속도로 복귀
+        Debug.Log("스피드업");
+    }
+    private void ResetSpeed()   // 속도를 원래대로 초기화
+    {
+        forwardSpeed = forwardSpeed_before; // 원래 속도로 복귀(규태님 나중에 2f 대신 플레이어 기본 속도로 넣어주세여)
+    }
+
+    //플레이어 클래스에 넣어주기
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;  // 체력 감소
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0; // 체력이 0 이하로 내려가지 않도록 방지
+        }
+        Debug.Log("데미지");
+    }
+
+    //플레이어 클래스에 넣어주기
+    public IEnumerator TakePoisonDamage(int damage, int times, float interval)  // 일정 시간마다 HP 감소하는 코루틴
+    {
+        for (int i = 0; i < times; i++)
+        {
+            TakeDamage(damage);
+            Debug.Log($"독 데미지! 피해 {damage}, 현재 체력 {currentHealth}, 남은 횟수 {times - i - 1}번");
+            yield return new WaitForSeconds(interval);  // n초(interval) 대기
+        }
+    }
+
+
+
+
+
+    void Jump()
+    {
+        animator.SetBool("IsJump", true);
+        if (jumpCount == 0)
+        {
+            // _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, playerJumpPower); // 1단 점프
+            _rigidbody.AddForce(Vector3.up * playerJumpPower, ForceMode2D.Impulse);
+        }
+
+        else if (jumpCount == 1)
+        {
+            _rigidbody.AddForce(Vector3.up * playerJumpPower, ForceMode2D.Impulse); // 2단 점프 힘 증가
+        }
+    }
+    private void Slide(bool isSlide)
+    {
+        slide = isSlide;
+        animator.SetBool("IsSlide", isSlide); // 애니메이션 설정
+    }
+
+
+
+    bool CheckGround()
+    {
+        float rayLength = 2.5f; // 레이저 길이 증가
+        LayerMask groundLayer = LayerMask.GetMask("Ground"); // 바닥 그라운드 레이어
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, rayLength, groundLayer);
+        Debug.DrawRay(transform.position, Vector2.down * rayLength, Color.red);
+        lastPosition_Y = transform.position.y;
+        if (hit.collider != null && transform.position.y <= lastPosition_Y) //땅에 닿고 있고 +내려가는중
+        {
+            animator.SetBool("IsJump", false);
+            // Physics2D.Raycast 물리충돌을 감지하는 레이저 발사 , 플레이어 위치에서 레이저 발사
+            // Vector2.down 아래 방향으로 0.3f 만큼의 길이를 발사. 바닥이 있는지 검사
+            return false; // 바닥이 감지되면 점프 초기화
+
+        }
+        else // 여기는 땅에 안닿고있고 올라가는중?
+        {
+            animator.SetBool("IsJump", true);
+            return true;
+        }
+
+
+    }
 }
