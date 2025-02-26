f75b4cbc9724ce083aaae8043bf9b7ed3d0126cf
pxo2 <rlarbxodia@gmail.com>
2025-02-26 10:52:31 +0900

    Update_tae
    슬라이드+보성님 점프 해결

--- Assets/Scripts/BaseController.cs
+++ Assets/Scripts/BaseController.cs
@@ -1,26 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class BaseController : MonoBehaviour
-{
-    protected Rigidbody2D _rigidbody;
+//public class BaseController : MonoBehaviour
+//{
+//    protected Rigidbody2D _rigidbody;
 
-    [SerializeField] private SpriteRenderer characterRenderer;
+//    [SerializeField] private SpriteRenderer characterRenderer;
 
-    protected AnimationHandler animationHandler;
+//    protected AnimationHandler animationHandler;
 
-    protected virtual void Awake()
-    {
-        _rigidbody = GetComponent<Rigidbody2D>();
-        animationHandler = GetComponent<AnimationHandler>();
-    }
+//    protected virtual void Awake()
+//    {
+//        _rigidbody = GetComponent<Rigidbody2D>();
+//        animationHandler = GetComponent<AnimationHandler>();
+//    }
 
-    //private void Movment(Vector2 direction)
-    //{
-    //    direction = direction * 5;
+//    private void Movment(Vector2 direction)
+//    {
+//        direction = direction * 5;
 
-    //    _rigidbody.velocity = direction;
-    //    animationHandler.Jump(direction);
-    //}
-}
+//        _rigidbody.velocity = direction;
+//        animationHandler.Jump(direction);
+//    }
+//}
